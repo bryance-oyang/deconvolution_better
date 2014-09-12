efc94f2cda8a25a19929b94c68ab37e5f5d920f6
Bryance Oyang <bobianite@gmail.com>
2014-09-12 13:40:55 -0700

    added in vars image_a image_b and their complex counterparts

--- deconvolute.c
+++ deconvolute.c
@@ -13,17 +13,21 @@
 /********************************/
 
 static int width, height;
-static uint16_t *input_image;
-static uint8_t *psf_image;
+static uint16_t *original_input_image;
+static uint8_t *original_psf_image;
 
 /* real images */
-static float *norm_input_image[3];
-static float *norm_current_image[3];
-static float *norm_output_image[3];
-static float *norm_psf_image[3];
+static float *input_image[3];
+static float *current_image[3];
+static float *output_image[3];
+static float *psf_image[3];
+static float *image_a[3];
+static float *image_b[3];
 
 /* complex images */
-
+static float *cimage_a[3][2];
+static float *cimage_b[3][2];
+static float *cimage_psf[3][2];
 
 /* fftw vars */
 static fftwf_plan fft_forward_plan;
@@ -41,6 +45,14 @@ static cl_kernel mult_k[3];
 static cl_kernel complex_mult_k[3];
 static cl_kernel complex_conj_mult_k[3];
 static cl_kernel divide_k[3];
+/* wait (sync) events */
+static cl_event kernel_events[3];
+/* opencl memory buffers */
+static cl_mem k_image_a[3];
+static cl_mem k_image_b[3];
+static cl_mem k_cimage_a[3][2];
+static cl_mem k_cimage_b[3][2];
+static cl_mem k_cimage_psf[3][2];
 
 /* functions */
 static int init_images(char *input_image_filename, char
@@ -99,46 +111,71 @@ int init_images(char *input_image_filename, char *psf_image_filename)
 	int x, y, index, psf_index;
 
 	/* read in images */
-	input_image = read_tiff16(input_image_filename, &width,
+	original_input_image = read_tiff16(input_image_filename, &width,
 			&height);
-	if (input_image == NULL)
+	if (original_input_image == NULL)
 		goto out_no_input_image;
 
-	psf_image = read_tiff8(psf_image_filename, &psf_width,
+	original_psf_image = read_tiff8(psf_image_filename, &psf_width,
 			&psf_height);
-	if (psf_image == NULL)
+	if (original_psf_image == NULL)
 		goto out_no_psf_image;
 
 	/* alloc memory for images */
 	for (c = 0; c < 3; c++) {
-		norm_input_image[c] = malloc(width * height *
-				sizeof(*norm_input_image[c]));
-		norm_current_image[c] = malloc(width * height *
-				sizeof(*norm_current_image[c]));
-		norm_output_image[c] = malloc(width * height *
-				sizeof(*norm_output_image[c]));
-		norm_psf_image[c] = calloc(width * height,
-				sizeof(*norm_psf_image[c]));
+		input_image[c] = malloc(width * height *
+				sizeof(*input_image[c]));
+		current_image[c] = malloc(width * height *
+				sizeof(*current_image[c]));
+		output_image[c] = malloc(width * height *
+				sizeof(*output_image[c]));
+		psf_image[c] = calloc(width * height,
+				sizeof(*psf_image[c]));
+		image_a[c] = malloc(width * height *
+				sizeof(*image_a[c]));
+		image_b[c] = malloc(width * height *
+				sizeof(*image_b[c]));
 
-		if (norm_input_image[c] == NULL)
+		if (input_image[c] == NULL)
 			goto out_nomem;
-		if (norm_current_image[c] == NULL)
+		if (current_image[c] == NULL)
 			goto out_nomem;
-		if (norm_output_image[c] == NULL)
+		if (output_image[c] == NULL)
 			goto out_nomem;
-		if (norm_psf_image[c] == NULL)
+		if (psf_image[c] == NULL)
 			goto out_nomem;
+		if (image_a[c] == NULL)
+			goto out_nomem;
+		if (image_b[c] == NULL)
+			goto out_nomem;
+
+		/* alloc memory for complex images */
+		for (i = 0; i < 2; i++) {
+			cimage_a[c][i] = malloc(width * (height/2 + 1) *
+					sizeof(*cimage_a[c][i]));
+			cimage_b[c][i] = malloc(width * (height/2 + 1) *
+					sizeof(*cimage_b[c][i]));
+			cimage_psf[c][i] = malloc(width * (height/2 + 1)
+					* sizeof(*cimage_psf[c][i]));
+
+			if (cimage_a[c][i] == NULL)
+				goto out_nomem;
+			if (cimage_b[c][i] == NULL)
+				goto out_nomem;
+			if (cimage_psf[c][i] == NULL)
+				goto out_nomem;
+		}
 	}
 
 	/* convert input image over to float */
 	for (i = 0; i < 3 * width * height; i++) {
-		norm_input_image[i%3][i/3] = (float)input_image[i]/UINT16_MAX;
-		norm_current_image[i%3][i/3] = (float)input_image[i]/UINT16_MAX;
+		input_image[i%3][i/3] = (float)original_input_image[i]/UINT16_MAX;
+		current_image[i%3][i/3] = (float)original_input_image[i]/UINT16_MAX;
 	}
 
 	float total[3] = {0, 0, 0};
 	for (i = 0; i < 3 * psf_width * psf_height; i++) {
-		total[i%3] += (float)psf_image[i];
+		total[i%3] += (float)original_psf_image[i];
 	}
 
 	/* copy psf over to padded float psf image */
@@ -150,8 +187,8 @@ int init_images(char *input_image_filename, char *psf_image_filename)
 				index = y * width + x;
 				psf_index = 3 * (j * psf_width + i) + c;
 
-				norm_psf_image[c][index] = (float)
-					psf_image[psf_index]/total[c];
+				psf_image[c][index] = (float)
+					original_psf_image[psf_index]/total[c];
 			}
 		}
 	}
@@ -160,18 +197,31 @@ int init_images(char *input_image_filename, char *psf_image_filename)
 
 out_nomem:
 	for (c = 0; c < 3; c++) {
-		if (norm_input_image[c] != NULL)
-			free(norm_input_image[c]);
-		if (norm_current_image[c] != NULL)
-			free(norm_current_image[c]);
-		if (norm_output_image[c] != NULL)
-			free(norm_output_image[c]);
-		if (norm_psf_image[c] != NULL)
-			free(norm_psf_image[c]);
+		if (input_image[c] != NULL)
+			free(input_image[c]);
+		if (current_image[c] != NULL)
+			free(current_image[c]);
+		if (output_image[c] != NULL)
+			free(output_image[c]);
+		if (psf_image[c] != NULL)
+			free(psf_image[c]);
+		if (image_a[c] != NULL)
+			free(image_a[c]);
+		if (image_b[c] != NULL)
+			free(image_b[c]);
+
+		for (i = 0; i < 2; i++) {
+			if (cimage_a[c][i] != NULL)
+				free(cimage_a[c][i]);
+			if (cimage_b[c][i] != NULL)
+				free(cimage_b[c][i]);
+			if (cimage_psf[c][i] != NULL)
+				free(cimage_psf[c][i]);
+		}
 	}
-	free(psf_image);
+	free(original_psf_image);
 out_no_psf_image:
-	free(input_image);
+	free(original_input_image);
 out_no_input_image:
 	fprintf(stderr, "init_images: failed\n");
 	fflush(stderr);
@@ -180,17 +230,25 @@ out_no_input_image:
 
 void cleanup_init_images()
 {
-	int c;
+	int c, i;
 
 	for (c = 0; c < 3; c++) {
-		free(norm_psf_image[c]);
-		free(norm_output_image[c]);
-		free(norm_current_image[c]);
-		free(norm_input_image[c]);
+		free(psf_image[c]);
+		free(output_image[c]);
+		free(current_image[c]);
+		free(input_image[c]);
+		free(image_a[c]);
+		free(image_b[c]);
+
+		for (i = 0; i < 2; i++) {
+			free(cimage_a[c][i]);
+			free(cimage_b[c][i]);
+			free(cimage_psf[c][i]);
+		}
 	}
 
-	free(psf_image);
-	free(input_image);
+	free(original_psf_image);
+	free(original_input_image);
 }
 
 /*
@@ -252,7 +310,7 @@ void cleanup_init_fftw()
 int init_opencl()
 {
 	int ret;
-	int c;
+	int c, i;
 
 	/* set global work size */
 	global_work_size[0] = width * height;
@@ -285,10 +343,60 @@ int init_opencl()
 	}
 
 	/* allocate opencl buffers */
+	for (c = 0; c < 3; c++) {
+		k_image_a[c] = clCreateBuffer(context,
+				CL_MEM_READ_WRITE, width * height *
+				sizeof(cl_float), NULL, NULL);
+		k_image_b[c] = clCreateBuffer(context,
+				CL_MEM_READ_WRITE, width * height *
+				sizeof(cl_float), NULL, NULL);
 
+		if (k_image_a[c] == NULL)
+			goto out_nomem;
+		if (k_image_b[c] == NULL)
+			goto out_nomem;
+
+		/* allocate complex buffers */
+		for (i = 0; i < 2; i++) {
+			k_cimage_a[c][i] = clCreateBuffer(context,
+					CL_MEM_READ_WRITE, width *
+					(height/2 + 1) *
+					sizeof(cl_float), NULL, NULL);
+			k_cimage_b[c][i] = clCreateBuffer(context,
+					CL_MEM_READ_WRITE, width *
+					(height/2 + 1) *
+					sizeof(cl_float), NULL, NULL);
+			k_cimage_psf[c][i] = clCreateBuffer(context,
+					CL_MEM_READ_WRITE, width *
+					(height/2 + 1) *
+					sizeof(cl_float), NULL, NULL);
+
+			if (k_cimage_a[c][i] == NULL)
+				goto out_nomem;
+			if (k_cimage_b[c][i] == NULL)
+				goto out_nomem;
+			if (k_cimage_psf[c][i] == NULL)
+				goto out_nomem;
+		}
+	}
 
 	return 0;
 
+out_nomem:
+	for (c = 0; c < 3; c++) {
+		if (k_image_a[c] != NULL)
+			clReleaseMemObject(k_image_a[c]);
+		if (k_image_b[c] != NULL)
+			clReleaseMemObject(k_image_b[c]);
+		for (i = 0; i < 2; i++) {
+			if (k_cimage_a[c][i] != NULL)
+				clReleaseMemObject(k_cimage_a[c][i]);
+			if (k_cimage_b[c][i] != NULL)
+				clReleaseMemObject(k_cimage_b[c][i]);
+			if (k_cimage_psf[c][i] != NULL)
+				clReleaseMemObject(k_cimage_psf[c][i]);
+		}
+	}
 out_no_kernel:
 	for (c = 0; c < 3; c++) {
 		if (mult_k[c] != NULL)
@@ -311,7 +419,18 @@ out_gpu_fail:
 
 void cleanup_init_opencl()
 {
-	int c;
+	int c, i;
+
+	for (c = 0; c < 3; c++) {
+		clReleaseMemObject(k_image_a[c]);
+		clReleaseMemObject(k_image_b[c]);
+
+		for (i = 0; i < 2; i++) {
+			clReleaseMemObject(k_cimage_a[c][i]);
+			clReleaseMemObject(k_cimage_b[c][i]);
+			clReleaseMemObject(k_cimage_psf[c][i]);
+		}
+	}
 
 	for (c = 0; c < 3; c++) {
 		clReleaseKernel(mult_k[c]);
